0a4aab59d34cef6f3bddfc463cb92d7521ec98ad
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-04-30 09:58:12 -0700

    Fix bug where gulp was not showing lint output

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
 gulp.task("lint", function() {
-    gulp.src([ "client/**/*.js", "!client/lib/**" ])
+    // return the stream so that the process is kept alive until done.
+    return gulp.src([ "client/**/*.js", "!client/lib/**" ])
       .pipe(eslint())
       .pipe(eslint.format());
 });
